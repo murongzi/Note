ec23503065f02f22780b20f76745fd79ab9451d4
mazidong <mazidong@jk.cn>
2017-04-24 00:25:14 +0800

    提交关于babel的stage几个阶段的整理

---
+++ 2017/babel-demo/dist/variable/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var _regenerator = require('babel-runtime/regenerator');
+
+var _regenerator2 = _interopRequireDefault(_regenerator);
+
+var _context;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _marked = [foo].map(_regenerator2.default.mark);
+
+var aa = '100';
+
+var bb = '100';
+
+var cc = '100';
+
+var a = function a() {
+    console.log('test');
+};
+
+var o = {
+    bb: function bb() {
+        console.log('test');
+    }
+};
+
+var x = function () {
+    var tmp = f();
+    return tmp * tmp + 1;
+}();
+
+(_context = [123, 321, 4231], console.log).bind(_context);
+
+(_context = (_context = (_context = (_context = [1, 2, 3, 4], map).call(_context, function (x) {
+    return x.character();
+}), takeWhile).call(_context, function (x) {
+    return x.strength > 100;
+}), forEach).call(_context, function (x) {
+    return console.log(x);
+}), console.log).bind(_context);
+
+function foo() {
+    return _regenerator2.default.wrap(function foo$(_context2) {
+        while (1) {
+            switch (_context2.prev = _context2.next) {
+                case 0:
+                case 'end':
+                    return _context2.stop();
+            }
+        }
+    }, _marked[0], this);
+}
+//# sourceMappingURL=index.js.map
