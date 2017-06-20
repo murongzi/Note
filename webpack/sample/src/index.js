c3dd3c45de201268590590aad37ce22f5dd18afc
mazidong <mazidong@jk.cn>
2017-06-20 22:48:34 +0800

    提交

--- webpack/sample/src/index.js
+++ webpack/sample/src/index.js
@@ -2,8 +2,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import {Router, Route, IndexRoute, hashHistory, Link, Redirect} from 'react-router';
-import Hello from './main';
+import {Router, Route, IndexRoute, browserHistory, hashHistory, Link, Redirect} from 'react-router';
 
 var Test = class extends React.Component {
     constructor(props) {
@@ -12,11 +11,13 @@ var Test = class extends React.Component {
 
     render() {
         return (
-            <div>Test
-                <a href="/">去首页</a>xxxx
-                <a href="users">去用户页</a>
-                哈哈哈哈哈哈哈哈哈 about页面
-            </div>
+            <App>
+                <div>Test页
+                    <a href="/">去首页</a>xxxx
+                    <a href="users">去用户页</a>
+                    哈哈哈哈哈哈哈哈哈 about页面
+                </div>
+            </App>
         );
     }
 };
@@ -26,28 +27,66 @@ var App = class extends React.Component {
     constructor(props) {
         super(props)
     }
-    
-
 
     render() {
         return (
             <div>
-                main page
-                <a href="#/about">Click about</a>xxxx
-                <a href="#/users">Click users</a>
+                {this.props.children || ''}
             </div>
         );
     }
 };
 
+var Hello =  class extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <App>
+                Hello页面
+                <a href="/">去首页</a>xxxx
+                <a href="about">去about页</a>
+            </App>
+        );
+    }
+};
+
+var Home = class extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <App>
+                Home页
+                <a href="about">about页面</a>xxxx
+                <a href="users">去用户页</a>
+            </App>
+        );
+    }
+};
+
 render((
+    <Router history={browserHistory}>
+        <Route path='/' component={App}>
+            <IndexRoute component={Home}/>
+            <Route path='about' component={Test} />
+            <Route path='users' component={Hello} />
+        </Route>
+    </Router>
+), document.querySelector('#rootApp'));
+
+/*render((
     <Router>
         <Route path="/" component={App}>
             <Route path="/about" component={Test}/>
             <Route path="/users" component={Hello}/>
         </Route>
     </Router>
-), document.querySelector('#rootApp'));
+), document.querySelector('#rootApp'));*/
 
 /*render((
     <Router>
--- webpack/sample/src/main.js
+++ webpack/sample/src/main.js
@@ -1,20 +1,4 @@
-/*window.autoExec((o) => {
-    const {React, ReactDOM} = o;
 
-    class MyComponent extends React.Component {
-        constructor(props) {
-            super(props);
-        }
-
-        render() {
-            return (
-                <div>子组件哈哈哈哈哈哈哈哈哈哈哈</div>
-            );
-        }
-    }
-
-    return MyComponent;
-});*/
 import React from 'react';
 export default class extends React.Component {
     constructor(props) {
