24647734be2becb94e6860e4150d839c9b5d1112
mazidong <mazidong@jk.cn>
2017-06-23 14:12:32 +0800

    提交代码

--- redux/sample-04/src/index.js
+++ redux/sample-04/src/index.js
@@ -7,21 +7,6 @@ import Home from './containers/Home';
 import App from './containers/App';
 import Detail from './containers/Detail';
 
-/*render(
-    (
-        <Provider store={store}>
-            <Router history={browserHistory}>
-                <Route path="/" component={App}>
-                    <IndexRoute component={Home}/>
-                    <Route path="detail" component={Detail}/>
-                </Route>
-            </Router>
-        </Provider>
-    ),
-    document.getElementById('rootApp')
-)
-*/
-
 render(
     (
         <Provider store={store}>
